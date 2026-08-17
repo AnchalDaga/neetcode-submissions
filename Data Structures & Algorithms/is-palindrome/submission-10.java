629cb49f55a0f980feaf25e522c38d0572b515dd
Anchal Daga <94402269+AnchalDaga@users.noreply.github.com>
2026-08-17 17:02:31 -0400

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        
+
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().trim();
+        int n = s.length();
+
+        if(s.isEmpty()) {
+            return true;
+        }
+
+        if (n == 1) {
+            return true;
+        }
+        
+        int l = n-1;
+        boolean res = false;
+
+        for(int i=0;i <= l;i++){
+            while(l >= i){
+                if(s.charAt(i)==s.charAt(l)){
+                    res = true;
+                }
+                else{
+                    return false;
+                    
+                }
+                l--;
+                break;
+            }
+        }
+        return res;
+
+
+        
+    }
+}
